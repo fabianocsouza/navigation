629862d8e4cdf5a2dbca5e8c680b0b900c83e0d7
Fabiano C. Souza <fabiano1403@hotmail.com>
2025-06-04 08:00:55 -0300

    create components `Header`,`Title`, `ButtonIcon` and page `Home`,`Product`

---
+++ src/app/Product.tsx
@@ -0,0 +1,16 @@
+import { View } from "react-native";
+
+import { Title } from "./components/Title";
+import { Header } from "./components/Header";
+import { ButtonIcon } from "./components/ButtonIcon";
+
+export function Product() {
+  return (
+    <View style={{ flex: 1, padding: 32, paddingTop: 80 }}>
+      <Header>
+        <ButtonIcon name="arrow-circle-left" />
+        <Title>Product</Title>
+      </Header>
+    </View>
+  );
+}
